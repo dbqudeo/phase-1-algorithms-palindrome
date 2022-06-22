17b69b176cde9ab43901e372b9c56665f8964523
byeongdae yu <brianbdyu@gmail.com>
2022-06-22 19:28:45 -0400

    commit tree 640cb8cb4b870328c32fc63cbd74dbcd7c8c23b4

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string) {
+    // convert string to an array
+    const arrayValues = string.split('');
+
+    // reverse the array values
+    const reverseArrayValues = arrayValues.reverse();
+
+    // convert array to string
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        return true
+    }
+    else {
+        return false
+    }
 }
+
 
 /* 
   Add your pseudocode here
